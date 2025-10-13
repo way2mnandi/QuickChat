59aa12969ddf958e601e244752a8bef8b2b645fe
Bokang Mmako <st10477570@rcconnect.edu.za>
2025-10-13 20:57:38 +0000

    update sendMessage method removed malicious sendMessage tests(require JUnit input)

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -12,6 +12,7 @@ import java.util.Random;
 
 import javax.swing.JOptionPane;
 public class Message {
+    int Option;
     String input;
     int totalMessagesSent;
     String RecepientcellNo;
@@ -19,15 +20,15 @@ public class Message {
     int messagesSent;
     int messageNumber = 0;
     String message;
-    int retry = 3;
+    int retry = 3; //inistializing variables and constants
     boolean messageLength = false;
     ArrayList<String> storedMessages = new ArrayList<>();
     ArrayList<String> messageIDs = new ArrayList<>();
-    ArrayList<String> messageHashes = new ArrayList<>();
+    ArrayList<String> messageHashes = new ArrayList<>(); //arraylist logic to prep for part 3
     StringBuilder info = new StringBuilder();
     boolean sessionActive = true;
     int over;
-    public void mainMenu(){
+    public void mainMenu(){ //action selection menu
         Object[] options = {"    1    ","    2    ","   3    "};
         int menuSelection = JOptionPane.showOptionDialog(null, "Select An Option: \n1. Send Messages\n2. Display old messages\n3.Quit",
         "Welcome to QuickChat",
@@ -41,11 +42,11 @@ public class Message {
                 communicate();
                 break;
             case 1:
-                JOptionPane.showMessageDialog(null,"Coming Soon.");
+                JOptionPane.showMessageDialog(null,"Coming Soon."); //part 3 feature
                 break;
             case 2:
                 sessionActive = false;
-                System.exit(0);
+                System.exit(0); //exits program when user selects exit
             default:
                 break;
         }
@@ -55,12 +56,12 @@ public class Message {
         while(retry > 0){
         RecepientcellNo = JOptionPane.showInputDialog("Insert recepient cellphone number.");
         if(checkRecepientCell(RecepientcellNo)){
-            JOptionPane.showMessageDialog(null, "Recepient number successfully captured");
-            retry =3;
+            JOptionPane.showMessageDialog(null, "Recepient number successfully captured"); //prompts user for necessary info
+            retry =3; //resets retry variable after successful data capture
             break;
         }
         else{ 
-            retry --;
+            retry --; //increment to reduce try amounts
             if(retry > 0){
             JOptionPane.showMessageDialog(null,"Try again, "+retry+" attempt(s) remaining!","ALERT!",JOptionPane.ERROR_MESSAGE);
             }
@@ -68,17 +69,17 @@ public class Message {
         }
         if(retry <= 0){
             JOptionPane.showMessageDialog(null,"Too many attempts, try again later.","PROGRAM EXITING...",JOptionPane.ERROR);
-            System.exit(0);
+            System.exit(0); //exits program on too many attempts
         }
         recepientName = JOptionPane.showInputDialog("Insert Recepient name: ");
-         input = JOptionPane.showInputDialog("How many messages would you like to send?");
-        messagesSent = Integer.parseInt(input);
+         input = JOptionPane.showInputDialog("How many messages would you like to send?"); 
+        messagesSent = Integer.parseInt(input); //parses message amount to int(Joption only allows String entries)
         for (int i = 0; i < messagesSent; i++){
-        writeMessage();
+        writeMessage(); //calls for message writing method for every message consecutively and exits loop when complete
         }
 
     }
-    public static boolean checkRecepientCell(String recepientNo){
+    public static boolean checkRecepientCell(String recepientNo){ //module to validate recepient phone number
        boolean format = false;
         boolean length = false;
         recepientNo = recepientNo.replaceAll("[\\s-]", ""); //removes number spaces
@@ -88,43 +89,43 @@ public class Message {
         }
         return length&&format;
     }
-    public String createMessageID(){
+    public String createMessageID(){ //generates message    ID's using random creation
       boolean idValid = false;
       Random ID = new Random();
       StringBuilder messageID = new StringBuilder();
-      while(!idValid){
-      long id = 1000000000L +(long)(ID.nextDouble()*8999999999L);
-      String TempmessageID = String.valueOf(id);
+      while(!idValid){ //ensures correct id is generated, runs until it is
+      long id = 1000000000L +(long)(ID.nextDouble()*8999999999L); //sets lenght and value rules for auto generated id
+      String TempmessageID = String.valueOf(id); //sends id to another string to validate
       if(checkMessageID(TempmessageID)){
       messageID.append(TempmessageID);
       messageIDs.add(TempmessageID);
       idValid = true;
       }
       else{
-        System.out.println("ID Generator failed, trying again....");
+        System.out.println("ID Generator failed, trying again...."); //retries id generation on fail
       }
       }
       return messageID.toString();
     }
-    public static boolean checkMessageID(String checkID){
-        return checkID.length()<=10;
+    public static boolean checkMessageID(String checkID){ //checks id length
+        return checkID.length() ==10;
     }
-    public String createMessageHash(String message){
-        String[] hashWords = message.split(" ");
-        messageNumber ++;
+    public String createMessageHash(String message){ //creates message hash
+        String[] hashWords = message.split(" "); //removes  spaces in the messages to prepare for hash generatiin 
+        messageNumber ++; //increments message number to generate correct hash
         return createMessageID().substring(0,2)+":"+messageNumber+":"+hashWords[0]+hashWords[hashWords.length-1];
     }
-    public void writeMessage(){
-        while(messagesSent > 0){
+    public void writeMessage(){ //method to write the message
+        while(messagesSent > 0){ //runs until all required messages are processed
         message = JOptionPane.showInputDialog("INSERT YOUR MESSAGE HERE : ");
-        message = message.toUpperCase();
+        message = message.toUpperCase(); //converts message to uppercase for the hash
         sendMessage();
-        messagesSent --;
+        messagesSent --; //increment to accommodate while loop
         }
     }
-    public String sendMessage(){
-        Object[] options = {"   1   ","   2   ","   3   "};
-        int Option = JOptionPane.showOptionDialog(null,
+    public String sendMessage(){ //message action menu
+        Object[] options = {"   1   ","   2   ","   3   "}; //array of options
+        this.Option = JOptionPane.showOptionDialog(null,  //pane with options for actions
         "1.Send Message\n2.Store Message\n3.Delete Message",
         "Choose what to do with your message",
         JOptionPane.YES_NO_CANCEL_OPTION,
@@ -133,10 +134,10 @@ public class Message {
         options, options[0]);
         switch(Option){
             case 0:
-            JOptionPane.showMessageDialog(null, checkMessageLength(message));
-            totalMessagesSent++;
-            storedMessages.add(message);
-            messageHashes.add(createMessageHash(message));
+            JOptionPane.showMessageDialog(null, checkMessageLength(message)); //calls message length  checker
+            totalMessagesSent++; //increment for final window display
+            storedMessages.add(message); //adds the message into array
+            messageHashes.add(createMessageHash(message)); //adds message hash into hash array
              //for future reference in part 3(arrays)
                storeMessage();
                return "Message Sent Successfully";
@@ -149,7 +150,7 @@ public class Message {
                return "Message Successfully stored";
         }
     }
-    public String checkMessageLength(String message){
+    public String checkMessageLength(String message){ //ensures message length is not above 50
          if(message.length()>=250){
             messageLength = false;
             this.over = message.length() - 250;
@@ -202,7 +203,7 @@ public class Message {
         
 
     }
-    public String printMessage(){
+    public String printMessage(){ //prints the SENT messages and leaves out stored messages
         StringBuilder sentMessages = new StringBuilder();
         for(int i = 0; i < storedMessages.size();i++){
             sentMessages.append("ID: ").append(messageIDs.get(i)).append(" | Message: ").append(storedMessages.get(i)).append("\n");
@@ -210,7 +211,7 @@ public class Message {
         return recepientName+"("+RecepientcellNo+")\n"+sentMessages.toString();
     }
     public int returnTotalMessages(){
-        return totalMessagesSent;
+        return totalMessagesSent; //shows how many messages were sent
     }
 }
 
--- src/test/java/MessageTest.java
+++ src/test/java/MessageTest.java
@@ -6,31 +6,37 @@ public class MessageTest {
     @Test
     void TestRecepientCellValidator(){
         assertTrue(Message.checkRecepientCell("+27718693002\r"));
-        assertFalse(Message.checkMessageID("08575975889"));
+        assertFalse(Message.checkMessageID("08575975889")); //tests cell no validation
     }
     @Test
-    void testMessageLengthSuccess(){
+    void testMessageIDvalidator(){
+        Message msg = new Message();
+        assertTrue(Message.checkMessageID(msg.createMessageID()));
+        assertFalse(Message.checkMessageID("000000"));
+    }
+    @Test
+    void testMessageLengthSuccess(){ //tests message length validator on success
         Message msg = new Message();
         String expectedMessage = "Message ready to send.";
         String result = msg.checkMessageLength("Hi Mike, can you join us for dinner tonight");
         assertEquals(expectedMessage, result);
     }
     @Test
-    void testMessageLengthFailure(){
+    void testMessageLengthFailure(){ //tests message length validation on failure
         Message msg = new Message();
         String expectedMessage = "Message exceeds 250 characters by "+10+", please reduce size";
         String result = msg.checkMessageLength("A".repeat(260));
         assertEquals(expectedMessage, result);
     }
     @Test
-    void messageHashSuccess(){
+    void messageHashSuccess(){ //tests message hash creation
         Message msg = new Message();
         String expectedMessage = String.format("00:0:HITONIGHT");
         String result = msg.createMessageHash("Hi Mike, can you join us for dinner tonight").toUpperCase().replaceAll("\\d{2}:\\d", "00:0");
         assertEquals(expectedMessage, result);
     }
     @Test
-    void testMessageIDGeneration(){
+    void testMessageIDGeneration(){ //tests id generation
         Message msg = new Message();
         String expectedMesssage = "0000000000";
         String result = msg.createMessageID().replaceAll("\\d{10}", "0000000000");
